a56b0153e8d594f4fff9d9bbc0169658cda197de
xoma_star <sasha.sirotyin09@gmail.com>
2022-06-21 10:25:38 +0500

    cors covers and audio

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,12 @@ import * as cookieParser from "cookie-parser";
 
 const start = async () => {
   try {
-    const PORT = process.env.PORT || 5000
+    const PORT = 5000
     const app = await NestFactory.create(AppModule)
-    const whitelist = ['http://localhost:3000', 'https://cd-rom.vercel.app', 'http://localhost:5000', 'http://192.168.1.24:3000']
+    const whitelist = ['http://localhost:3000', 'https://serenity.xoma-star.space', 'http://localhost:5000', 'http://192.168.1.24:3000']
     app.use(cookieParser())
     app.enableCors({credentials: true, origin: function (origin, callback) {
-        if (whitelist.indexOf(origin) !== -1 || typeof origin === 'undefined') {
+        if (whitelist.indexOf(origin) !== -1) {
           callback(null, true)
         } else {
           callback(new Error('Not allowed by CORS'))
