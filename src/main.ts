80bda9ba16a893615ca8390248d67762ff948e50
xoma_star <40947642+xoma-star@users.noreply.github.com>
2023-05-24 10:49:56 +0500

    new domain
    new d

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ const start = async () => {
     try {
         const PORT = 5000
         const app = await NestFactory.create(AppModule)
-        const whitelist = ['http://localhost:3000', 'https://serenity.xoma-star.space', 'http://localhost:5000', 'http://192.168.1.24:3000']
+        const whitelist = ['http://localhost:3000', 'https://serenity.xoma-star.space', 'http://localhost:5000', 'http://192.168.1.24:3000', 'https://serenity.xoma-star.tk']
         app.use(cookieParser())
         app.enableCors({credentials: true, origin: function (origin, callback) {
             if (whitelist.indexOf(origin) !== -1 || typeof origin === 'undefined') callback(null, true)
